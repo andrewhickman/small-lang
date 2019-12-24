120eee056a4403eb19be4dcc7e24a58ec3761975
Andrew Hickman <andrew.hickman1@sky.com>
2019-12-24 23:14:32 +0000

    Fix build script causing unconditional recompilation

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
 fn main() {
-    println!("cargo:rerun-if-changed=/src/syntax/parser.lalrpop");
+    println!("cargo:rerun-if-changed=src/syntax/parser.lalrpop");
     lalrpop::process_root().unwrap()
 }
