993fc936ef20e6d6f88ae84602ece3b9059e0daf
Andrew Hickman <andrew.hickman1@sky.com>
2019-12-26 21:12:59 +0000

    Add wasm runtime compilation to build script

--- build.rs
+++ build.rs
@@ -1,4 +1,25 @@
+use std::error::Error;
+use std::path::PathBuf;
+use std::{env, fs};
+
 fn main() {
+    build_parser().unwrap();
+    build_wasm().unwrap();
+}
+
+fn build_parser() -> Result<(), Box<dyn Error>> {
     println!("cargo:rerun-if-changed=src/syntax/parser.lalrpop");
-    lalrpop::process_root().unwrap()
+    lalrpop::process_root().unwrap();
+    Ok(())
+}
+
+fn build_wasm() -> Result<(), Box<dyn Error>> {
+    let src = "src/wasm/rt.wat";
+    let dst = PathBuf::from(env::var_os("OUT_DIR").ok_or("`OUT_DIR` not set")?).join("rt.wasm");
+
+    println!("cargo:rerun-if-changed={}", src);
+    let wat = fs::read(src)?;
+    let wasm = wabt::wat2wasm(wat)?;
+    fs::write(dst, wasm)?;
+    Ok(())
 }
---
+++ src/wasm/rt.wat
@@ -0,0 +1,6 @@
+(module
+  (func $alloc (result i32)
+    (i32.const 0)
+  )
+  (export "alloc" (func $alloc))
+)
