4bdb17f82eb55a614e0e3c54fe1943e12d526dd3
Andrew Hickman <andrew.hickman1@sky.com>
2020-01-11 12:30:16 +0000

    Add memory_copy operation

--- src/wasm/rt.wat
+++ src/wasm/rt.wat
@@ -1,12 +1,10 @@
 (module
   (memory 1)
-  ;; Allocator
-  ;;
-  ;; The position of the bump-allocator.
   (global $brk (mut i32) (i32.const 0))
-  (func $alloc (param $size i32) (result i32)
+  ;; Simple bump allocator
+  (func $memory_alloc (param $len i32) (result i32)
     (global.get $brk) ;; The return value
-    (global.set $brk (i32.add (global.get $brk) (local.get $size))) ;; Update allocator position
+    (global.set $brk (i32.add (global.get $brk) (local.get $len))) ;; Update allocator position
 
     (if
       (i32.eq
@@ -26,19 +24,25 @@
       )
     )
   )
-  (func $pair_first (param $record i64) (result i32)
-    (i32.wrap_i64
-      (i64.shr_u
-        (local.get $record)
-        (i64.const 32)
+  ;; Copy `len` bytes from `src` to `dst`.
+  (func $memory_copy (param $src i32) (param $dst i32) (param $len i32)
+    (local $src_end i32)
+
+    (local.set $src_end (i32.add (local.get $src) (local.get $len)))
+
+    (block
+      (loop
+        (br_if 1 (i32.eq (local.get $src) (local.get $src_end)))
+
+        (i32.store8 (local.get $dst) (i32.load8_u (local.get $src)))
+
+        (local.set $src (i32.add (local.get $src) (i32.const 1)))
+        (local.set $dst (i32.add (local.get $dst) (i32.const 1)))
+        (br 0)
       )
     )
   )
-  (func $pair_second (param $record i64) (result i32)
-    (i32.wrap_i64
-      (local.get $record)
-    )
-  )
   (export "memory" (memory 0))
-  (export "alloc" (func $alloc))
+  (export "memory_alloc" (func $memory_alloc))
+  (export "memory_copy" (func $memory_copy))
 )
--- src/wasm/tests.rs
+++ src/wasm/tests.rs
@@ -28,17 +28,31 @@ impl Runtime {
             .unwrap()
     }
 
-    fn alloc(&self, size: i32) -> Result<i32, Error> {
-        Ok(self
-            .instance
-            .invoke_export("alloc", &[RuntimeValue::I32(size)], &mut NopExternals)?
-            .unwrap()
-            .try_into()
-            .unwrap())
+    fn memory_alloc(&self, size: i32) -> Result<i32, Error> {
+        let result = self.instance.invoke_export(
+            "memory_alloc",
+            &[RuntimeValue::I32(size)],
+            &mut NopExternals,
+        )?;
+        Ok(result.unwrap().try_into().unwrap())
+    }
+
+    fn memory_copy(&self, src: i32, dst: i32, len: i32) -> Result<(), Error> {
+        let result = self.instance.invoke_export(
+            "memory_copy",
+            &[
+                RuntimeValue::I32(src),
+                RuntimeValue::I32(dst),
+                RuntimeValue::I32(len),
+            ],
+            &mut NopExternals,
+        )?;
+        assert!(result.is_none());
+        Ok(())
     }
 }
 
-mod alloc {
+mod memory_alloc {
     use wasmi::memory_units::Pages;
     use wasmi::LINEAR_MEMORY_PAGE_SIZE;
 
@@ -48,9 +62,9 @@ mod alloc {
     fn bump() {
         let rt = Runtime::instantiate();
 
-        assert_eq!(rt.alloc(5).unwrap(), 0);
-        assert_eq!(rt.alloc(10).unwrap(), 5);
-        assert_eq!(rt.alloc(6).unwrap(), 15);
+        assert_eq!(rt.memory_alloc(5).unwrap(), 0);
+        assert_eq!(rt.memory_alloc(10).unwrap(), 5);
+        assert_eq!(rt.memory_alloc(6).unwrap(), 15);
     }
 
     #[test]
@@ -59,13 +73,44 @@ mod alloc {
         let mem = rt.memory();
 
         assert_eq!(mem.current_size(), Pages(1));
-        rt.alloc(20).unwrap();
+        rt.memory_alloc(20).unwrap();
         assert_eq!(mem.current_size(), Pages(1));
-        rt.alloc(LINEAR_MEMORY_PAGE_SIZE.0 as i32 * 1).unwrap();
+        rt.memory_alloc(LINEAR_MEMORY_PAGE_SIZE.0 as i32 * 1)
+            .unwrap();
         assert_eq!(mem.current_size(), Pages(2));
-        rt.alloc(40).unwrap();
+        rt.memory_alloc(40).unwrap();
         assert_eq!(mem.current_size(), Pages(2));
-        rt.alloc(LINEAR_MEMORY_PAGE_SIZE.0 as i32 * 2).unwrap();
+        rt.memory_alloc(LINEAR_MEMORY_PAGE_SIZE.0 as i32 * 2)
+            .unwrap();
         assert_eq!(mem.current_size(), Pages(4));
+    }
+}
+
+mod memory_copy {
+    use super::Runtime;
+
+    #[test]
+    fn string() {
+        let rt = Runtime::instantiate();
+        let mem = rt.memory();
+
+        const HELLO: &'static str = "Hello, world!";
+        let src = rt.memory_alloc(HELLO.len() as i32).unwrap();
+        let dst = rt.memory_alloc(HELLO.len() as i32).unwrap();
+        mem.set(src as u32, HELLO.as_bytes()).unwrap();
+
+        rt.memory_copy(src, dst, HELLO.len() as i32).unwrap();
+
+        assert_eq!(mem.get(dst as u32, HELLO.len()).unwrap(), HELLO.as_bytes());
+    }
+
+    #[test]
+    fn empty() {
+        let rt = Runtime::instantiate();
+
+        let src = rt.memory_alloc(1).unwrap();
+        let dst = rt.memory_alloc(2).unwrap();
+
+        rt.memory_copy(src, dst, 0).unwrap();
     }
 }
